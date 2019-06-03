281c9d72a7496475e5be39417e04b24b5810c90f
Tebello NAMO <tnamo@c4r18s1.wethinkcode.co.za>
2019-06-03 16:38:24 +0200

    first date with C++

---
+++ DAYOO/EX00/megaphone.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int    main(int argc, char **argv)
+{
+    int i;
+
+    i = 0;
+    if (argc == 1) {
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+        return (0);
+    }
+    while ()
+    std::cout << argv[1] << std::endl;
+    return (0);
+}
